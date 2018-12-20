c493ada96c6c574407aa9c6de2bb2495aaaae0e0
Kevin Cornea <kpcornea@gmail.com>
2018-12-19 22:53:58 -0800

    Add united_we_stand

--- lib/02_select_countries.rb
+++ lib/02_select_countries.rb
@@ -78,5 +78,11 @@ end
 def united_we_stand
   # Show the countries that have a name that includes the word 'United'
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      name LIKE '%United%'
   SQL
 end
