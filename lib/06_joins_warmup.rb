ebe544b318b5656f14823ba9832c4171206e2aa6
Kevin Cornea <kpcornea@gmail.com>
2018-12-20 13:39:59 -0800

    Add alien_cast

--- lib/06_joins_warmup.rb
+++ lib/06_joins_warmup.rb
@@ -127,5 +127,17 @@ end
 def alien_cast
   # Obtain the cast list for the film 'Alien'
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      actors
+    JOIN
+      castings
+      ON actors.id = castings.actor_id
+    JOIN
+      movies
+      ON movies.id = castings.movie_id
+    WHERE
+      title = 'Alien'
   SQL
 end
