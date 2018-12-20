2b9d1fae47ac3e954a9a5e1479d19b3bca33aa3c
Kevin Cornea <kpcornea@gmail.com>
2018-12-20 12:16:13 -0800

    Add country_counts

--- lib/05_aggregates.rb
+++ lib/05_aggregates.rb
@@ -68,6 +68,12 @@ end
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+    SELECT
+      continent, COUNT(name)
+    FROM
+      countries
+    GROUP BY
+      continent
   SQL
 end
 
