f28dda4c9d0f4451e1084c6728b854140bb6e92a
Kevin Cornea <kpcornea@gmail.com>
2018-12-20 13:55:52 -0800

    Add travoltas_busiest_years

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -94,6 +94,20 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    SELECT
+      yr, COUNT(title)
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      name = 'John Travolta'
+    GROUP BY
+      yr
+    HAVING
+      COUNT(title) >= 2
   SQL
 end
 
